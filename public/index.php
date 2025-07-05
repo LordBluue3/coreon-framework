c60c861f553da64b11f9315329712eca8e5cacd8
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2025-07-04 22:32:16 -0300

    chore: edit the public index

--- public/index.php
+++ public/index.php
@@ -1,14 +1,3 @@
 <?php
 
 require "../boostrap.php";
-
-use Core\Controller;
-
-try{
-    $controller = new Controller;
-
-    $controller = $controller->load();
-    dd($controller);
-}catch(\Exception $e){
-    dd($e->getMessage());
-}
