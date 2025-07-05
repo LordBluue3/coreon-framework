9f99328a9be3eb153b3c8ebfa74a8326be499cf0
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2025-07-04 22:34:20 -0300

    feat: create controllers folder

---
+++ app/Controllers/Api/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use App\Controllers\BaseController;
+
+class TestController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/ContatoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class ContatoController extends BaseController
+{
+    public function index()
+    {
+        echo "oi";
+    }
+    public function enviar()
+    {
+        echo "oi";
+    }
+}
